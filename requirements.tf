a11b73ec6eb893bef9127d3faae63527507e9b84
Asier Perez <aasier@gmail.com>
2025-03-17 22:04:39 +0100

    requirements

--- requirements.tf
+++ requirements.tf
@@ -1,1 +1,17 @@
 # provider requirements
+terraform {
+  required_version = ">= 1.9" 
+}
+
+terraform {
+  required_providers {
+    external = {
+      source = "hashicorp/external"
+      version = "2.3.4"
+    }
+  }
+}
+
+provider "external" {
+  # Configuration options
+}
